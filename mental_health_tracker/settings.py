253ffe3600c2a7bd730b64cac5a6c5ddaed02342
lillyskyy <nandociyek@gmail.com>
2024-09-17 18:46:04 +0700

    coba pws lagi

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://https://ananda-dwi31-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://https://ananda-dwi31-mentalhealthtracker.pbp.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://ananda-dwi31-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://ananda-dwi31-mentalhealthtracker.pbp.cs.ui.ac.id/"]
