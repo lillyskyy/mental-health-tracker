be26aa753b144c34ccc021e50121aba3d5be2dba
lillyskyy <nandociyek@gmail.com>
2024-09-17 18:24:33 +0700

    rill akhir

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from djang5o.urls import path
 from main.views import show_main, create_mood_entry, show_xml, show_json, show_xml_by_id, show_json_by_id
 app_name = 'main'
 
